c23e3f9df4f766b4810feb8144224277416eb0f0
Star1xDev <abdastar2@gmail.com>
2022-08-09 14:52:24 +0100

    finished exercice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+      return "ERROR";
+    }
+    let text = "";
+    for (var i = 0; i < number; i++) {
+      text += string;
+    }
+    return text;
 };
 
 // Do not edit below this line
