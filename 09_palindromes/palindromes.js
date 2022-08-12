83afdad528366ce0e55ef52984b54e11b73ca84c
Star1xDev <abdastar2@gmail.com>
2022-08-12 15:39:48 +0100

    finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const stringChars = string.split("");
+  const stringLetters = stringChars.filter(char => char.toLowerCase() !== char.toUpperCase()
+  );
+  // console.log(stringLetters);
+  const cleanString = stringLetters.join("");
+  // console.log(cleanString);
 
+  for (var i = 0; i < Math.floor(cleanString.length / 2); i++) {
+    if (cleanString[i].toLowerCase() !== cleanString[cleanString.length - 1 - i].toLowerCase()){
+        return false;
+    }
+  }
+  return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
