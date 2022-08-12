444b40738e70a3c2a5163f946409d74b9b1afb3f
Star1xDev <abdastar2@gmail.com>
2022-08-12 15:04:19 +0100

    finished exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const sum = array.reduce((total,currentNumber) => {
+      return total + currentNumber;
+  },0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const result = array.reduce((total,currentNumber) => {
+      return total * currentNumber;
+  }, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+    return number**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+    let fact = 1;
+    while (number > 0) {
+      fact = fact * number--;
+    }
+    return fact;
 };
 
 // Do not edit below this line
