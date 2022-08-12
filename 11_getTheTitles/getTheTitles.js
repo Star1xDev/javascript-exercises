152902bf4c54d6ecf2e4a5f73572b731834e0cb7
Star1xDev <abdastar2@gmail.com>
2022-08-12 15:58:57 +0100

    finished exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  const titles = array.map(book => book.title);
+  return titles;
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
